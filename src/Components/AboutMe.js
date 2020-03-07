124403a01a243afcb00edc61ba77dc6d0763a642
andy <andy.william.mcdonald@gmail.com>
2020-03-07 23:29:26 +0000

    picture in place in about me section

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -6,22 +6,46 @@ import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
+import AndyPic from '../Assets/about-me-images-svgs/Andy_McDonald.jpg';
 
 const useStyles = makeStyles({
   root: {
     height: "90vh",
-    textAlign: "center"
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    // padding: 10
+  },
+  text:{
+    paddingBottom: 20,
+  },
+  media:{
+    // height: 'auto',
+    // width: '50%',
+    height: '45%',
+    width: 'auto',
+    maxWidth: '350px',
+    // maxHeight: '350px',
+    // padding: '10%',
+    // paddingTop: '5%',
+    borderRadius: '50%'
   }
 });
 
 function AboutMe() {
   const classes = useStyles();
   return (
-    // <Paper className={classes.root} elevation={2}>
-    <Card>
-      <Typography variant="h3">About me</Typography>
+    <Card className={classes.root} elevation={2}>
+      <Typography className={classes.text} variant="h3">About me</Typography>
+      <CardMedia
+        className={classes.media}
+        src={AndyPic}
+        component= 'img'
+        title="Headshot"
+        // xs={8}
+        // sm={2}
+      />
     </Card>
-    // </Paper>
-  );
+    );
 }
 export default AboutMe;
