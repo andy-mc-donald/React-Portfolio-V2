c9dc6801808cb99796aa3b88272c3bc63c96aa46
andy <andy.william.mcdonald@gmail.com>
2020-03-20 14:23:27 +0000

    footer has social links and form

--- src/Pages/Page4.js
+++ src/Pages/Page4.js
@@ -2,21 +2,61 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 // import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 import IconButton from "@material-ui/core/IconButton";
-import GitHubIcon from '@material-ui/icons/GitHub';
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GetAppIcon from "@material-ui/icons/GetApp";
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    backgroundColor: "blue",
-    height: "60vh",
-    color: "white"
+    backgroundColor: "pink",
+    // height: "60vh",
+    // textAlign: 'center'
   },
   container: {
-    padding: "2rem"
+    padding: "2rem",
   },
   item: {
-    textAlign: 'center'
+    // textAlign: "center",
+    // display: 'flex',
+    // justifyContent: "center",
+    // alignItems: 'center'
+    // margin: '0 auto'
+  },
+  card: {
+    display: "flex",
+    flexDirection: "row",
+    // justifyContent: "center", 
+    alignItems: 'center',
+    backgroundColor: 'pink',
+    boxShadow: 'none',
+    // paddingLeft: '30%',
+    height: '3rem',
+    // width: '50%',
+  },
+  cardCont: {
+    // display: "flex",
+    // flexDirection: "column",
+    // padding: 20
+  },
+
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    margin: 20,
+    padding: 20,
+  }, 
+
+  button: {
+    width: '20%',
   }
 }));
 
@@ -27,20 +67,75 @@ function Page4() {
     <div className={classes.root}>
       <Grid container className={classes.container}>
         <Grid item xs={12} sm={6} className={classes.item}>
-          <h2>Contact</h2>
-          <IconButton>
-              <GitHubIcon />
-          </IconButton>
-          <p>Blah blah blah</p>
-          <p>Blah blah blah</p>
-          <p>Blah blah blah blah ah </p>
+          <div className={classes.cardCont}>
+          <Card className={classes.card}>
+            <div>
+              <CardContent>
+                <IconButton>
+                  <GitHubIcon />
+                </IconButton>
+              </CardContent>
+            </div>
+            <div>
+              <CardContent>
+                <h3>GitHub</h3>
+              </CardContent>
+            </div>
+          </Card>
+
+          <Card className={classes.card}>
+            <div>
+              <CardContent>
+                <IconButton>
+                  <LinkedInIcon />
+                </IconButton>
+              </CardContent>
+            </div>
+            <div>
+              <CardContent>
+                <h3>LinkedIn</h3>
+              </CardContent>
+            </div>
+          </Card>
+
+          <Card className={classes.card}>
+            <div>
+              <CardContent>
+                <IconButton>
+                  <GetAppIcon />
+                </IconButton>
+              </CardContent>
+            </div>
+            <div>
+              <CardContent>
+                <h3>CV</h3>
+              </CardContent>
+            </div>
+          </Card>
+          </div>
         </Grid>
+
         <Grid item xs={12} sm={6} className={classes.item}>
-          <h3>different text</h3>
-          <p>Blah blah blah</p>
-          <p>Blah blah blah</p>
-          <p>Blah blah blah</p>
-          <p>Blah blah blah</p>
+          <div className={classes.form}>
+          <FormControl>
+            <InputLabel htmlFor="name">Name</InputLabel>
+            <Input id="name" type="text" />
+          </FormControl>
+
+          <FormControl>
+            <InputLabel htmlFor="email">Email</InputLabel>
+            <Input id="email" type="email" />
+          </FormControl> 
+
+          <FormControl>
+            <InputLabel htmlFor="email">Message</InputLabel>
+            <Input id="email" multiline rows={10} />
+          </FormControl>
+
+          <Button className={classes.button} variant="contained" color="primary" size="medium">
+            Send
+          </Button>
+          </div>
         </Grid>
       </Grid>
     </div>
