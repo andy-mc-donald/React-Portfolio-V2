ee7ebf3687ea4ae42a30e8d34659281e320d0bd6
andy <andy.william.mcdonald@gmail.com>
2020-03-25 13:48:23 +0000

    first elements going on page

--- src/Pages/Page1pt2.js
+++ src/Pages/Page1pt2.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import IconButton from "@material-ui/core/IconButton";
 import AndyPic from "../Assets/about-me-images-svgs/Andy_McDonald.jpg";
+import Button from "@material-ui/core/Button";
 import { ReactComponent as CSS3 } from "../Assets/tech-stack-icons/CSS3.svg";
 import { ReactComponent as GitHub } from "../Assets/tech-stack-icons/GitHub.svg";
 import { ReactComponent as HTML5 } from "../Assets/tech-stack-icons/HTML5.svg";
@@ -15,22 +17,41 @@ import { ReactComponent as FAC } from "../Assets/about-me-images-svgs/FAC_logo.s
 import { ReactComponent as FCC } from "../Assets/about-me-images-svgs/FreeCodeCamp.svg";
 import { ReactComponent as CodeWars } from "../Assets/about-me-images-svgs/Codewars_logo.svg";
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    background: "rgba(153, 184, 195, 0.17)",
+    height: "100vh",
+    flexGrow: 1
+  },
+  container: {
+    // width: "100%",
+    // margin: "2.5rem",
+    // ["@media (max-width:600px)"]: {
+    //   margin: "0.5rem"
+    // }
+  },
+  githubicon: {
+    fontSize: "xl",
+    marginLeft: "auto",
+    flexDirection: 'flex-end'
+    // margin: "2.5rem",
+  }
+}));
 
-const useStyles = makeStyles(theme =>({
-    root:{
-        background: 'rgba(153, 184, 195, 0.17)',
-        height: '100vh',
-    },
-}))
+function Page1pt2() {
+  const classes = useStyles();
 
-function Page1pt2(){
-    const classes = useStyles();
-
-    return(
-        <div className={classes.root}>
-            
-        </div>
-    )
+  return (
+    <div className={classes.root}>
+      <Grid container className={classes.container}>
+        <Grid item>
+          <IconButton aria-label="github" className={classes.githubicon}>
+            <GitHubIcon />
+          </IconButton>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default Page1pt2;
