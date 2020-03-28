4ff8d874d0f8a36c826b943b0c838160d7677a44
andy <andy.william.mcdonald@gmail.com>
2020-03-28 19:14:54 +0000

    media queries for large text

--- src/Pages/Page1pt2.js
+++ src/Pages/Page1pt2.js
@@ -5,23 +5,27 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import IconButton from "@material-ui/core/IconButton";
 import AndyPic from "../Assets/about-me-images-svgs/Andy_McDonald.jpg";
 import Button from "@material-ui/core/Button";
-import { ReactComponent as CSS3 } from "../Assets/tech-stack-icons/CSS3.svg";
-import { ReactComponent as GitHub } from "../Assets/tech-stack-icons/GitHub.svg";
-import { ReactComponent as HTML5 } from "../Assets/tech-stack-icons/HTML5.svg";
-import { ReactComponent as JavaScript } from "../Assets/tech-stack-icons/JavaScript.svg";
-import { ReactComponent as Linux } from "../Assets/tech-stack-icons/Linux.svg";
-import { ReactComponent as NodeJS } from "../Assets/tech-stack-icons/NodeJS.svg";
-import { ReactComponent as ReactLogoSVG } from "../Assets/tech-stack-icons/React.svg";
-import { ReactComponent as Sass } from "../Assets/tech-stack-icons/Sass.svg";
-import { ReactComponent as FAC } from "../Assets/about-me-images-svgs/FAC_logo.svg";
-import { ReactComponent as FCC } from "../Assets/about-me-images-svgs/FreeCodeCamp.svg";
-import { ReactComponent as CodeWars } from "../Assets/about-me-images-svgs/Codewars_logo.svg";
+// import { ReactComponent as CSS3 } from "../Assets/tech-stack-icons/CSS3.svg";
+import CSS3 from "../Assets/tech-stack-icons/CSS3.svg";
+import GitHub from "../Assets/tech-stack-icons/GitHub.svg";
+import HTML5 from "../Assets/tech-stack-icons/HTML5.svg";
+import JavaScript from "../Assets/tech-stack-icons/JavaScript.svg";
+import Linux from "../Assets/tech-stack-icons/Linux.svg";
+import NodeJS from "../Assets/tech-stack-icons/NodeJS.svg";
+import ReactLogoSVG from "../Assets/tech-stack-icons/React.svg";
+import Sass from "../Assets/tech-stack-icons/Sass.svg";
+import FAC from "../Assets/about-me-images-svgs/FAC_logo.svg";
+import FCC from "../Assets/about-me-images-svgs/FreeCodeCamp.svg";
+import CodeWars from "../Assets/about-me-images-svgs/Codewars_logo.svg";
 
 const useStyles = makeStyles(theme => ({
   root: {
     background: "rgba(153, 184, 195, 0.17)",
     height: "100vh",
     flexGrow: 1
+  },
+  githubiconContainer: {
+    // height: '5vh'
   },
   githubicon: {
     // margin: "2.5rem",
@@ -30,7 +34,9 @@ const useStyles = makeStyles(theme => ({
     flexDirection: "flex-end"
   },
   mainContainer: {
-    height: "60vh"
+    height: "65vh",
+    // maxheight: "100vh"
+    // overflow: "hidden"
   },
   picContainer: {
     overflow: "hidden"
@@ -44,16 +50,144 @@ const useStyles = makeStyles(theme => ({
   text: {
     // margin: 'auto',
     padding: "0px",
-    fontSize: "2.5rem",
-    "@media (max-width:600px)": {
-      fontSize: "1.1rem"
-    }
+    fontSize: "2.6vw",
+    "@media (max-width:1824px)": {
+      fontSize: "2.8vw"
+    },
+    "@media (max-width:1500px)": {
+      fontSize: "2.85vw"
+    },
+    "@media (max-width:1320px)": {
+      fontSize: "3vw"
+    },
+    "@media (max-width:1240px)": {
+      fontSize: "3.2vw"
+    },
+    "@media (max-width:1164px)": {
+      fontSize: "3.3vw"
+    },
+    "@media (max-width:1068px)": {
+      fontSize: "3.4vw"
+    },
+    "@media (max-width:940px)": {
+      fontSize: "3.5vw"
+    },
+    "@media (max-width:876px)": {
+      fontSize: "3.65vw"
+    },
+    "@media (max-width:772px)": {
+      fontSize: "3.8vw"
+    },
+    "@media (max-width: 706px)": {
+      fontSize: "4vw"
+    },
+    "@media (max-width: 690px)": {
+      fontSize: "4.2vw"
+    },
+    "@media (max-width: 636px)": {
+      fontSize: "4.4vw"
+    },
+    "@media (max-width: 573px)": {
+      fontSize: "4.6vw"
+    },
+    "@media (max-width: 522px)": {
+      fontSize: "4.75vw"
+    },
+    "@media (max-width: 474px)": {
+      fontSize: "4.9vw"
+    },
+    "@media (max-width: 420px)": {
+      fontSize: "5.1vw"
+    },
+    "@media (max-width: 390px)": {
+      fontSize: "5.3vw"
+    },
+    "@media (max-width: 375px)": {
+      fontSize: "5.5vw"
+    },
+    "@media (max-width: 356px)": {
+      fontSize: "5.7vw"
+    },
+    // "@media (max-width:1580px)": {
+    //   fontSize: "2.7rem"
+    // },
+    // "@media (max-width:1522px)": {
+    //   fontSize: "2.6rem"
+    // },
+    // "@media (max-width:1249px)": {
+    //   fontSize: "2.5rem"
+    // },
+    // "@media (max-width:1201px)": {
+    //   fontSize: "2.4rem"
+    // },
+    // "@media (max-width:1153px)": {
+    //   fontSize: "2.3rem"
+    // },
+    // "@media (max-width:1021px)": {
+    //   fontSize: "2.2rem"
+    // },
+    // "@media (max-width:976px)": {
+    //   fontSize: "2.1rem"
+    // },
+    // "@media (max-width:932px)": {
+    //   fontSize: "2.0rem"
+    // },
+    // "@media (max-width:750px)": {
+    //   fontSize: "1.9rem"
+    // },
+    // "@media (max-width:713px)": {
+    //   fontSize: "1.8rem"
+    // },
+    // "@media (max-width:675px)": {
+    //   fontSize: "1.7rem"
+    // },
+    // "@media (max-width:579px)": {
+    //   fontSize: "1.6rem"
+    // },
+    // "@media (max-width:535px)": {
+    //   fontSize: "1.55rem"
+    // },
+    // "@media (max-width:518px)": {
+    //   fontSize: "1.5rem"
+    // },
+    // "@media (max-width:501px)": {
+    //   fontSize: "1.45rem"
+    // },
+    // "@media (max-width:419px)": {
+    //   fontSize: "1.40rem"
+    // },   
+    // "@media (max-width:405px)": {
+    //   fontSize: "1.35rem"
+    // },
+    // "@media (max-width:390px)": {
+    //   fontSize: "1.3rem"
+    // },          
+
   },
   buttonContainer: {
-    height: "10vh",
-  }, 
+    height: "15vh"
+  },
   button: {
-    width: "30vw",
+    width: "40vw",
+    maxWidth: "500px",
+    fontSize: "1.5rem",
+    "@media (max-width:600px)": {
+        // width: "50vw",
+    fontSize: "0.9rem",
+    }
+  },
+  svgContainer:{
+      height: '15vh',
+  }, 
+  svgIcon:{
+    height: '80px',
+    width: 'auto',
+    padding: theme.spacing(1),
+  },
+  svgIcon2:{
+    height: '60px',
+    width: 'auto',
+    padding: theme.spacing(2),
   }
 }));
 
@@ -62,12 +196,7 @@ function Page1pt2() {
 
   return (
     <div className={classes.root}>
-      <Grid
-        container
-        direction="row"
-        justify="flex-end"
-        alignItems="center"
-      >
+      <Grid container direction="row" justify="flex-end" alignItems="center" className={classes.githubiconContainer}>
         <Grid item>
           <IconButton aria-label="github" className={classes.githubicon}>
             <GitHubIcon />
@@ -91,7 +220,7 @@ function Page1pt2() {
         </Grid>
       </Grid>
 
-      <Grid 
+      <Grid
         container
         direction="row"
         justify="center"
@@ -99,24 +228,41 @@ function Page1pt2() {
         className={classes.buttonContainer}
       >
         <Grid item>
-          <Button variant="contained" size="large" color="primary" href="" className={classes.button}>
+          <Button
+            variant="contained"
+            size="large"
+            color="primary"
+            href=""
+            buttonStyle={{ borderRadius: 50 }}
+            style={{ borderRadius: 50 }}
+            className={classes.button}
+          >
             Download CV
           </Button>
         </Grid>
       </Grid>
 
-      <Grid 
-       container
-       direction="row"
-       alignItems="center"
-       className={classes.svgContainer}
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        className={classes.svgContainer}
       >
-          <Grid item>
-
-          </Grid>
-          <Grid item>
-
-          </Grid>
+        <Grid item sm={8}>
+            <img src={HTML5} className={classes.svgIcon}></img>
+            <img src={CSS3} className={classes.svgIcon}></img>
+            <img src={JavaScript} className={classes.svgIcon}></img>
+            <img src={NodeJS} className={classes.svgIcon}></img>
+            <img src={ReactLogoSVG} className={classes.svgIcon}></img>
+            <img src={Sass} className={classes.svgIcon}></img>
+            <img src={GitHub} className={classes.svgIcon}></img>
+            <img src={Linux} className={classes.svgIcon}></img>
+        </Grid>
+        <Grid item sm={4}>
+            <img src={FAC} className={classes.svgIcon2}></img>
+            <img src={FCC} className={classes.svgIcon2}></img>
+            <img src={CodeWars} className={classes.svgIcon2}></img>
+        </Grid>
       </Grid>
     </div>
   );
